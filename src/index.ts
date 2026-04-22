e655781eb2d738145e560d77492b3e659640d43b
wanda azhar <wandaazhar@gmail.com>
2026-04-22 16:22:03 -0600

    add bind to host 127.0.0.1

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import { env } from "./config/env.js";
 
 const app = createApp();
 
-app.listen(env.PORT, () => {
-  console.log(`BACKEND-PORTFOLIO running on http://localhost:${env.PORT}`);
+// app.listen(env.PORT, () => {
+//   console.log(`BACKEND-PORTFOLIO running on http://localhost:${env.PORT}`);
+// });
+
+
+app.listen(env.PORT, "127.0.0.1", () => {
+  console.log(`Server BACKEND-PORTFOLIO-API running on http://127.0.0.1:${env.PORT}`);
 });
